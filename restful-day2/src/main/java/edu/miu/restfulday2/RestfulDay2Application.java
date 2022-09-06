83dbaa2096e399386a41f1298bc20c3651477d36
Sushil Subedi <sushil173@gmail.com>
2022-09-06 16:37:30 -0500

    project init

---
+++ restful-day2/src/main/java/edu/miu/restfulday2/RestfulDay2Application.java
@@ -0,0 +1,13 @@
+package edu.miu.restfulday2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulDay2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulDay2Application.class, args);
+	}
+
+}
