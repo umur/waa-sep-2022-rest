d62df3e84d3de082fcb6c4c62c0d2a1c5678baad
pradipdhungel <66682622+pradipdhungel@users.noreply.github.com>
2022-09-06 17:00:30 -0500

    all folder added

---
+++ day2/src/test/java/edu/miu/day2/Day2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.day2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
