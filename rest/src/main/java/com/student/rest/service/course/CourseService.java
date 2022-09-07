7941c48fb3a7dc3fdf3d240db6fc2d6199d1f976
asjadanis <asjadanis128@gmail.com>
2022-09-06 23:56:41 -0500

    chore: add course-service

---
+++ rest/src/main/java/com/student/rest/service/course/CourseService.java
@@ -0,0 +1,14 @@
+package com.student.rest.service.course;
+
+import com.student.rest.exceptions.CourseDoesNotExistsException;
+import com.student.rest.exceptions.course.CourseAlreadyExistsException;
+import com.student.rest.model.Course;
+
+import java.util.List;
+
+public interface CourseService {
+    public void add(Course course) throws CourseAlreadyExistsException;
+    public List<Course> get();
+    public boolean updateCourse (Course course) throws CourseDoesNotExistsException;
+    public boolean deleteCourse (int id) throws CourseDoesNotExistsException;
+}
